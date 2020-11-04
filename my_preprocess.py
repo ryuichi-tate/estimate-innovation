05413d70e88eaffd6a73f63ecb447fd24fbb48ce
ryuichi-tate <shield.dragon.one@gmail.com>
2020-11-04 18:31:29 +0900

    deviceをloaderに

--- my_preprocess.py
+++ my_preprocess.py
@@ -2,14 +2,15 @@ import numpy as np
 import torch
 
 class DataSet:
-    def __init__(self, tsData, hat_p):
+    def __init__(self, tsData, hat_p, device):
         """
         tsData：時系列データ（今回は一次元の時系列を想定）
         hat_p　：想定する次数
         initで入力データと出力データを作成する。
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True, drop_last=True)
         """
-        self.tsData =  torch.tensor(tsData, dtype=torch.float) # torch.tensorに変換
+        self.device = device
+        self.tsData =  torch.tensor(tsData, dtype=torch.float).to(self.device) # torch.tensorに変換
         self.hat_p = hat_p
         self.X, self.Y = self.make_XY(x=self.tsData, p=self.hat_p)
     
@@ -23,6 +24,7 @@ class DataSet:
         X = [x[i:i+p] for i in range(len(x)-p)]
         Y = [x[i+p] for i in range(len(x)-p)]
         return X, Y
+    
 
 def make_XY_forGandF(x:np.ndarray, p:int):
     """
